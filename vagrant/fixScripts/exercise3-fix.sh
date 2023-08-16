e3f79146d2e9e5f8e85b7cf15c8ddffdd393de1b
omerkenig <omerkenig@gmail.com>
2023-08-16 07:46:15 +0300

    Add exercise3-fix.sh

---
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#add fix to exercise3 here
+
+# Allow access to all and relod conf file
+ sudo sed -i 's/Require all denied/Require all granted/g' /etc/apache2/sites-available/000-default.conf
+ sudo systemctl reload apache2.service
