ce0382cce70546c7b75716d0e84140e0438bb592
omerkenig <omerkenig@gmail.com>
2023-08-16 09:03:34 +0300

    Update exercise4-fix_server1.sh and exercise4-fix_server2.sh

--- vagrant/fixScripts/exercise4-fix_server1.sh
+++ vagrant/fixScripts/exercise4-fix_server1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #add fix to exercise4-server2 here
 
-# add hostname for server1 ip 192.168.60.10
-echo "192.168.60.10 server1" | sudo tee -a /etc/hosts >> /dev/null
+# add hostname for server1 ip 192.168.60.11
+echo "192.168.60.11 server2" | sudo tee -a /etc/hosts >> /dev/null
--- vagrant/fixScripts/exercise4-fix_server2.sh
+++ vagrant/fixScripts/exercise4-fix_server2.sh
@@ -1,1 +1,5 @@
-echo "192.168.100.10 server1" | sudo tee -a /etc/hosts >> /dev/null
+#!/bin/bash
+#add fix to exercise4-server2 here
+
+# add hostname for server1 ip 192.168.60.10
+echo "192.168.60.10 server1" | sudo tee -a /etc/hosts >> /dev/null
